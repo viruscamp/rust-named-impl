a2bc102ec1602f639d30a783b650e22a34b3611a
VirusCamp <viruscamp@gmail.com>
2026-07-04 22:58:41 +0800

    rename Is functions use 'left' and 'right'

--- src/is.rs
+++ src/is.rs
@@ -1,51 +1,51 @@
 pub trait Is {
     type Type: ?Sized;
 
-    fn to_right(self) -> Self::Type where Self: Sized;
-    fn to_left(t: Self::Type) -> Self where Self: Sized;
-    fn to_ref_right(&self) -> &Self::Type;
-    fn to_ref_left(t: &Self::Type) -> &Self;
-    fn to_mut_right(&mut self) -> &mut Self::Type;
-    fn to_mut_left(t: &mut Self::Type) -> &mut Self;
+    fn to_right(left: Self) -> Self::Type where Self: Sized;
+    fn to_left(right: Self::Type) -> Self where Self: Sized;
+    fn to_ref_right(left: &Self) -> &Self::Type;
+    fn to_ref_left(right: &Self::Type) -> &Self;
+    fn to_mut_right(left: &mut Self) -> &mut Self::Type;
+    fn to_mut_left(right: &mut Self::Type) -> &mut Self;
 }
 
 impl<T: ?Sized> Is for T {
     type Type = T;
 
-    fn to_right(self) -> Self::Type where Self: Sized {
-        self
+    fn to_right(left: Self) -> Self::Type where Self: Sized {
+        left
     }
 
-    fn to_left(t: Self::Type) -> Self where Self: Sized {
-        t
+    fn to_left(right: Self::Type) -> Self where Self: Sized {
+        right
     }
 
-    fn to_ref_right(&self) -> &Self::Type {
-        self
+    fn to_ref_right(left: &Self) -> &Self::Type {
+        left
     }
 
-    fn to_ref_left(t: &Self::Type) -> &Self {
-        t
+    fn to_ref_left(right: &Self::Type) -> &Self {
+        right
     }
 
-    fn to_mut_right(&mut self) -> &mut Self::Type {
-        self
+    fn to_mut_right(left: &mut Self) -> &mut Self::Type {
+        left
     }
 
-    fn to_mut_left(t: &mut Self::Type) -> &mut Self {
-        t
+    fn to_mut_left(right: &mut Self::Type) -> &mut Self {
+        right
     }
 }
 
 // https://docs.rs/higher-kinded-types/latest/src/higher_kinded_types/advanced/type_eq.rs.html#99-100
 
 #[cfg(feature = "alloc")]
-pub fn into_box_val<T>(it: Box<T>) -> Box<<T as Is>::Type>
+pub fn to_box_left<T>(left: Box<T>) -> Box<<T as Is>::Type>
 {
-    it
+    left
 }
 
 #[cfg(feature = "alloc")]
-pub fn from_box_val<T>(x: Box<<T as Is>::Type>) -> Box<T> {
-    x
+pub fn to_box_right<T>(right: Box<<T as Is>::Type>) -> Box<T> {
+    right
 }
